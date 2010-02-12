eb13d30abe4a263d3633f0958f381f63665b14f8
Guillaume "dukez" Libersat <glibersat@sigill.org>
2010-02-13 00:44:41 +0100

    - Move bands to south - Remove junk from portlets test

--- apps/bands/admin.py
+++ apps/bands/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 
 from .models import Band, BandRole, BandMember
 
-from .myportlets import TextPortlet
-
-admin.site.register((Band, BandRole, BandMember, TextPortlet))
+admin.site.register((Band, BandRole, BandMember))
 
 
 
