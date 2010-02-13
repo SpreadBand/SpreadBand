4c76c0a3f25952a4bf815db26403e6360d461c02
Guillaume "dukez" Libersat <glibersat@sigill.org>
2010-02-13 13:26:36 +0100

    move portlet plugins for minisite in a separate package
    --HG--
    rename : apps/minisite/portlets/freetext/__init__.py => apps/minisite-portlets/__init__.py
    rename : apps/minisite/portlets/freetext/admin.py => apps/minisite-portlets/admin.py
    rename : apps/minisite/portlets/freetext/forms.py => apps/minisite-portlets/forms.py
    rename : apps/minisite/portlets/freetext/models.py => apps/minisite-portlets/models.py
    rename : apps/minisite/portlets/freetext/templates/portlets/freetext.html => apps/minisite-portlets/templates/portlets/freetext.html

---
+++ apps/minisite-portlets/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
+
+from minisite.models.portlet import Portlet
+from minisite.utils import register_portlet
+
+class FreetextPortlet(Portlet):
+    """
+    A simple portlet to display some text.
+    """
+    text = models.TextField(_(u'Text'), blank=True)
+
+    def __unicode__(self):
+        return '%s' % self.id
+
+    def render(self, context):
+        """
+        Renders the portlet as html.
+        """
+        return render_to_string('portlets/freetext.html',
+                                {'title' : self.title,
+                                 'text' : self.text
+                                 }
+                                )
+
+    def form(self, **kwargs):
+        """
+        """
+        from .forms import FreetextPortletForm
+        return FreetextPortletForm(instance=self, **kwargs)
+
+
+# Register this portlet
+register_portlet(FreetextPortlet, "FreetextPortlet")
