2c727cb4caddfaec90732b34337abbb0dcd8ef4f
Guillaume "dukez" Libersat <glibersat@sigill.org>
2010-04-26 09:22:28 +0200

    removed because of renaming

--- apps/bands/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Band, BandRole, BandMember
-
-admin.site.register((Band, BandRole, BandMember))
-
-
-
--- apps/gigplaces/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import GigPlace
-
-admin.site.register(GigPlace)
