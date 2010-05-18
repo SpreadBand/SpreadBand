5832e394f8a93fa4c5d3a06a68bd96856927e641
Guillaume "dukez" Libersat <glibersat@sigill.org>
2010-05-18 18:37:18 +0200

    enhance slugify to handle non ascii characters

--- media/js/jquery/jquery.slugify.js
+++ media/js/jquery/jquery.slugify.js
@@ -1,8 +1,11 @@
-jQuery.fn.slugify = function(obj) {
+jQuery.fn.slugify = function(obj)
+{
     jQuery(this).data('obj', jQuery(obj));
-    jQuery(this).keyup(function() {
-        var obj = jQuery(this).data('obj');
-        var slug = jQuery(this).val().replace(/\s+/g,'-').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase();
-        obj.val(slug);
-    });
+    jQuery(this).keyup(function()
+	{
+	    var obj = jQuery(this).data('obj');
+	    var slug = jQuery(this).val().replace(/[\'\s]+/g,'-').replace(/[éèê]/g, 'e').replace(/[âà]/g, 'a').replace(/[ç]/g, 'c').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase();
+	    obj.val(slug);
+	}
+    );
 }
