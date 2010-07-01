920da236c6c31a6630b69cd0c13ac44788deed79
Guillaume "dukez" Libersat <glibersat@sigill.org>
2010-07-01 11:04:25 +0200

    add testcase

--- apps/bargain/tests.py
+++ apps/bargain/tests.py
@@ -4,60 +4,60 @@ from django.test import TestCase
 from reversion import revision
 from reversion.models import Version
 
-from .models import Terms, Contract, Party
+from .models import Terms, Contract, Party, ContractParty
 
 class ContractTest(TestCase):
     def setUp(self):
-        terms = Terms()
-        terms.save()
-        self.c = Contract(terms=terms)
+        self.c = Contract()
         self.c.save()
 
-        user = User(pk=1)    
-        p = Party(contract=self.c, content_object=user)
+        terms = Terms(contract=self.c)
+        terms.save()
+
+        user = User(id=1)
+        p = Party(content_object=user)
         p.save()
 
+        cp = ContractParty(party=p, contract=self.c)
+        cp.save()
 
-    def testConcluded(self):
+    def testConcludedOne(self):
         """
         Test the dynamic concluded field
         """
-        self.assertEqual(self.c.concluded, False)
+        # test with one user
+        self.assertEqual(self.c.is_concluded, False)
 
-        self.c.concluded = True
-        self.assertEqual(self.c.concluded, True)
+        self.c.is_concluded = True
+        self.assertEqual(self.c.is_concluded, True)
 
         self.c.parties.all().delete()
-        self.assertEqual(self.c.concluded, True)
+        self.assertEqual(self.c.is_concluded, False)
 
-    def testVersions(self):
+    def testConcludedMultiple(self):
         """
-        Test the versioning mechanism
+        Test with 2 bargainers
         """
-        @revision.create_on_success
-        def make_changes(bool):
-            self.c.parties.all().update(approved=bool)
-            self.c.save()
+        # Create a second user
+        user2 = User(id=2)
+        p2 = Party(content_object=user2)
+        p2.save()
 
-        make_changes(False)
-        make_changes(True)
-        
-        versions = Version.objects.get_for_object(self.c)
-        self.assertEqual(len(versions), 2)
-        self.assertEqual(len(versions[0].revision.version_set.all()), 3)
+        cp2 = ContractParty(party=p2, contract=self.c)
+        cp2.save()
 
-        print self.c.parties.all()[0].approved
+        self.assertEqual(self.c.is_concluded, False)
 
-        old_contract = versions[0].get_object_version()
+        self.c.is_concluded = True
+        self.assertEqual(self.c.is_concluded, True)
 
-        old_party = versions[1].revision.version_set.all()[2]
+        cp2.approved = False
+        cp2.save()
 
-        #print old_party.field_dict
+        self.assertEqual(self.c.is_concluded, False)
 
-        old_contract = versions[0].object_version
+        cp2.approved = True
+        cp2.save()
 
-        
-        
+        self.assertEqual(self.c.is_concluded, True)
 
-
-        
