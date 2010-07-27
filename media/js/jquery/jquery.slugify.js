f77767b0b84b1457c28c16cb4bf3a0298da000b8
Guillaume "dukez" Libersat <guillaume@spreadband.com>
2010-07-27 19:07:19 +0200

    add a few letters to slugify

--- media/js/jquery/jquery.slugify.js
+++ media/js/jquery/jquery.slugify.js
@@ -4,7 +4,7 @@ jQuery.fn.slugify = function(obj)
     jQuery(this).keyup(function()
 	{
 	    var obj = jQuery(this).data('obj');
-	    var slug = jQuery(this).val().replace(/[\'\s]+/g,'-').replace(/[éèê]/g, 'e').replace(/[âà]/g, 'a').replace(/[ç]/g, 'c').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase();
+	    var slug = jQuery(this).val().replace(/[\'\s]+/g,'-').replace(/[üû]/g, 'u').replace(/[ëéèê]/g, 'e').replace(/[äâà]/g, 'a').replace(/[ç]/g, 'c').replace(/[^a-zA-Z0-9\-]/g,'').toLowerCase();
 	    obj.val(slug);
 	}
     );
