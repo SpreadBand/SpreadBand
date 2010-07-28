7cae5cfc0504442b846c7da0014ce6ccf01f7e89
Guillaume "dukez" Libersat <guillaume@spreadband.com>
2010-07-28 12:50:58 +0200

    moved gigbargain parts to another app : cleanup event app.

--- apps/event/admin.py
+++ apps/event/admin.py
@@ -2,19 +2,9 @@ from django.contrib import admin
 
 from reversion.admin import VersionAdmin
 
-
-from .models import GigBargain, GigBargainBand, Gig
-
-class GigBargainBandAdminInline(admin.StackedInline):
-    model = GigBargainBand
-    extra = 0
-
-class GigBargainAdmin(VersionAdmin):
-    inlines = (GigBargainBandAdminInline,)
+from .models import Gig
 
 admin.site.register(Gig)
-admin.site.register(GigBargain, GigBargainAdmin)
-admin.site.register(GigBargainBand, VersionAdmin)
 
 
 
--- apps/event/urls.py
+++ apps/event/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls.defaults import url, patterns
 from django.conf import settings
 
-import views.bargain.band
-import views.bargain.venue
-import views.bargain.dashboard
 import views.calendar
+import views.archive
 
 urlpatterns = patterns('django.views.generic.simple',
     (r'^$', 'redirect_to', {'url': 'list'}),
@@ -18,6 +16,9 @@ urlpatterns += patterns('',
     url(r'^gig/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/cancel$', views.calendar.gig_cancel, name='gig-cancel'),
     url(r'^gig/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/uncancel$', views.calendar.gig_uncancel, name='gig-uncancel'),
 
+    # Archive
+    url(r'^gigs/today$', views.archive.gigs_today, name='archive-gigs-today'),
+
     # Band calendar 
     url(r'^calendar/band/(?P<band_slug>[-\w]+).ics$', views.calendar.band_calendar_ics, name='band-calendar-ics'),
     url(r'^calendar/band/(?P<band_slug>[-\w]+)$', views.calendar.band_calendar_detail, name='band-calendar'),
@@ -25,51 +26,6 @@ urlpatterns += patterns('',
     # Venue calendar
     url(r'^calendar/venue/(?P<venue_slug>[-\w]+).ics$', views.calendar.venue_calendar_ics, name='venue-calendar-ics'),
     url(r'^calendar/venue/(?P<venue_slug>[-\w]+)$', views.calendar.venue_calendar_detail, name='venue-calendar'),
-
-
-    # Bargain
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)$', views.bargain.gigbargain_detail, name='gigbargain-detail'),
-
-    # Dashboards
-    url(r'^bargain/gig/band/dashboard/(?P<band_slug>[-\w]+)$', views.bargain.dashboard.gigbargain_band_dashboard, name='gigbargain-band-dashboard'),
-
-    # Bargain, comments
-    url(r'bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/comments/(?P<section>[-\w]+)$', views.bargain.comments_section_display, name='gigbargain-comments-section-display'),
-
-    # Bargain, venue specific
-    url(r'^bargain/gig/new/venue$', views.bargain.venue.gigbargain_new_from_venue, name='gigbargain-new-from-venue'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/venue/confirm_bands$', views.bargain.venue.gigbargain_venue_confirm_bands, name='gigbargain-venue-confirm-bands'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/venue/conclude$', views.bargain.venue.gigbargain_venue_conclude, name='gigbargain-venue-conclude'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/venue/decline$', views.bargain.venue.gigbargain_venue_decline, name='gigbargain-venue-decline'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/venue/enter$', views.bargain.venue.gigbargain_venue_enter_negociations, name='gigbargain-venue-enter'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/venue/renegociate', views.bargain.venue.gigbargain_venue_renegociate, name='gigbargain-venue-renegociate'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/venue/cancel', views.bargain.venue.gigbargain_venue_cancel, name='gigbargain-venue-cancel'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/venue/invite_band', views.bargain.venue.gigbargain_venue_invite_band, name='gigbargain-venue-invite-band'),
-
-    # Bargain, band specific
-    url(r'^bargain/gig/new/band/(?P<band_slug>[-\w]+)$', views.bargain.band.gigbargain_new_from_band, name='gigbargain-new-from-band'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/band/(?P<band_slug>[-\w]+)/enter$', views.bargain.band.gigbargain_enter_for_band, name='gigbargain-enter-for-band'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/band/(?P<band_slug>[-\w]+)/reject$', views.bargain.band.gigbargain_refuse_for_band, name='gigbargain-refuse-for-band'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/band/(?P<band_slug>[-\w]+)/quit$', views.bargain.band.gigbargain_band_quit, name='gigbargain-band-quit'),
-
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/band/(?P<band_slug>[-\w]+)/approve$', views.bargain.band.gigbargain_band_part_approve, name='gigbargain-band-part-approve'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/band/(?P<band_slug>[-\w]+)/edit$', views.bargain.band.gigbargain_band_part_edit, name='gigbargain-band-part-edit'),
-
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/band/(?P<band_slug>[-\w]+)$', views.bargain.band.gigbargain_band_part_display, name='gigbargain-band-part-display'),
-
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/submit_to_venue', views.bargain.band.gigbargain_band_submit_draft_to_venue, name='gigbargain-band-submit-to-venue'),
-
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/invite_band', views.bargain.band.gigbargain_band_invite_band, name='gigbargain-band-invite-band'),
-
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/renegociate', views.bargain.band.gigbargain_band_draft_renegociate, name='gigbargain-band-draft-renegociate'),
-
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/kick/(?P<band_slug>[-\w]+)', views.bargain.band.gigbargain_band_kick, name='gigbargain-band-kick'),
-
-
-
-    # Bargain, common parts
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/common/band/(?P<band_slug>[-\w]+)$', views.bargain.band.gigbargain_band_common_edit, name='gigbargain-band-common-edit'),
-    url(r'^bargain/gig/(?P<gigbargain_uuid>[\w\d-]+)/common/venue$', views.bargain.venue.gigbargain_venue_common_edit, name='gigbargain-venue-common-edit'),
 
 )
 
