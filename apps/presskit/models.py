9187cd7eaaa4f8b811982f39fdb577b7fd8a14a7
Guillaume "dukez" Libersat <guillaume@spreadband.com>
2010-07-29 18:23:55 +0200

    presskit++ and player on main page

--- apps/presskit/models.py
+++ apps/presskit/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
-from django.db.models import ForeignKey, ManyToManyField
+from django.db.models import ForeignKey, ManyToManyField, OneToOneField
 
 from band.models import Band
+from media.models import Track
 
 class PressKit(models.Model):
-    pass
+    tracks = ManyToManyField(Track, blank=True)
+    band = OneToOneField(Band, related_name='presskit')
+
+    def __unicode__(self):
+        return "PressKit for %s" % self.band.name
+
     
--- apps/presskit/views.py
+++ apps/presskit/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
-from band.models import Band
-
 from .models import PressKit
 
 def presskit_detail(request, band_slug):
-    band = get_object_or_404(Band, slug=band_slug)
+    presskit = get_object_or_404(PressKit, band__slug=band_slug)
 
     # Five latest gigs
-    latest_gigs = band.gigs.past_events()[:5]
+    latest_gigs = presskit.band.gigs.past_events()[:5]
     
-    extra_context = {'band': band,
+    extra_context = {'band': presskit.band,
+                     'presskit': presskit,
                      'latest_gigs': latest_gigs}
 
     return render_to_response(template_name='presskit/presskit_detail.html',
