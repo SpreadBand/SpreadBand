ca16e125940eadfed624fcc0be1a66b463e77c72
Guillaume "dukez" Libersat <guillaume@spreadband.com>
2010-08-04 17:01:52 +0200

    timeline for bands and venus

--- apps/event/views/calendar.py
+++ apps/event/views/calendar.py
@@ -84,17 +84,71 @@ from agenda.views import date_based
 from agenda.views.timeline import timeline
 from ..models import Gig
 
+#XXX Bad hack
+import imp
+fp, pathname, description = imp.find_module('calendar')
+pythoncal = imp.load_module('calendar', fp, pathname, description)
+from collections import defaultdict
+
+class GigMonthlyHTMLCalendar(pythoncal.HTMLCalendar):
+    """
+    An HTML calendar for gigs of a given month
+    """
+    def __init__(self, firstweekday, aQueryset, when):
+        pythoncal.HTMLCalendar.__init__(self, firstweekday)
+
+        # Prepare a date-indexed list of gigbargains
+        # XXX Maybe it's possible to do it using a query, but I don't know how
+        self._gigs = defaultdict(list)
+        for gig in aQueryset:
+            self._gigs[gig.event_date].append(gig)
+
+        self._when = when
+        
+    def formatday(self, day, weekday):
+        """
+        Return a day as a table cell.
+        """
+        if day == 0:
+            return '<td class="noday">&nbsp;</td>' # day outside month
+        else:
+            the_day = date(self._when.year, self._when.month, day)
+            if the_day in self._gigs:
+                cell = '<td class="%s"><font color="red">%d</font>' % (self.cssclasses[weekday], day)
+                cell += '<ul>'
+                for gig in self._gigs[the_day]:
+                    cell += '<li><a href="%s">%s</a></li>' % (gig.get_absolute_url(),
+                                                              gig.title)
+                cell += '</ul></td>'
+                return cell
+            else:
+                return '<td class="%s">%d</td>' % (self.cssclasses[weekday], day)
+
+    def toHTML(self):
+        return self.formatmonth(self._when.year, self._when.month)
+
+
 def band_calendar_detail(request, band_slug):
     """
     Show a calendar for a Band
     """
     band = get_object_or_404(Band, slug=band_slug)
 
+    today = date.today()
+    gigs = band.gigs.filter(event_date__year=today.year,
+                            event_date__month=today.month)
+
+    monthly_calendar = GigMonthlyHTMLCalendar(firstweekday=0,
+                                              aQueryset=gigs,
+                                              when=today)
+
     return timeline(request,
                     model=Gig,
                     calendar=band.calendar,
+                    past_event_count=1,
                     template_name='event/band_timeline.html',
-                    extra_context={'band': band},
+                    extra_context={'band': band,
+                                   'monthly_calendar': monthly_calendar},
                     )
 
 def band_calendar_ics(request, band_slug):
@@ -130,6 +184,7 @@ def venue_calendar_detail(request, venue_slug):
 
     return timeline(request,
                     model=Gig,
+                    past_event_count=1,
                     calendar=venue.calendar,
                     template_name="event/venue_timeline.html",
                     extra_context={'venue': venue},
