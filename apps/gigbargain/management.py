9cce06c1d095eea78dc43ad2a50316d2cd8e5225
Guillaume "dukez" Libersat <guillaume@spreadband.com>
2010-09-17 20:10:39 +0200

    change notice type

--- apps/gigbargain/management.py
+++ apps/gigbargain/management.py
@@ -7,7 +7,7 @@ from annoying.decorators import signals
 
 @signals(post_syncdb, sender=notification)
 def create_notice_types(app, created_models, verbosity, **kwargs):
-    notification.create_notice_type("gigbargain_proposal", _("New gig bargain proposal"), _("you have received a proposal for a gig bargain"))
+    notification.create_notice_type("gigbargain_invitation", _("Gig bargain invitation"), _("you have received an invitation to play"))
     notification.create_notice_type("gigbargain_new", _("New gig bargain"), _("you have received a gig bargain"))
     notification.create_notice_type("gigbargain_approved", _("Gig bargain approved"), _("A gig bargain was approved"))
     notification.create_notice_type("gigbargain_disapproved", _("Gig bargain disapproved"), _("A gig bargain was approved"))
