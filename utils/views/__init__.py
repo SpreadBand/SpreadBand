bb0851c95190abb8452622fac25884de345e3e8b
Guillaume "dukez" Libersat <guillaume@spreadband.com>
2011-01-12 02:07:28 +0100

    send to band if only one is managed

--- utils/views/__init__.py
+++ utils/views/__init__.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.forms import AuthenticationForm
@@ -9,7 +9,11 @@ def home_spreadband(request):
     else, show user's page
     """
     if request.user.is_authenticated and not request.user.is_anonymous():
-        return HttpResponseRedirect(reverse('account:detail', args=[request.user.username]))
+        # If we only have one band, then go to this page, else send to dashboard
+        if request.user.bands.count() == 1:
+            return redirect(request.user.bands.all()[0])
+        else:
+            return HttpResponseRedirect(reverse('account:detail', args=[request.user.username]))
     else:
         auth_form = AuthenticationForm()
 
