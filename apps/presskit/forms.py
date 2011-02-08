ec140c5b0d459ebca943712f8295ab7690ecf4bb
Guillaume "dukez" Libersat <guillaume@spreadband.com>
2011-02-09 00:04:49 +0100

    add a boolean check to prevent people from uploading if they don't declare to have the rights

--- apps/presskit/forms.py
+++ apps/presskit/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext as _
 from .models import PressKit, PresskitViewRequest
 
 from media.models import Track
@@ -11,7 +12,11 @@ class PressKitVideoForm(forms.ModelForm):
 class PressKitTrackForm(forms.ModelForm):
     class Meta:
         model = Track
-        fields = ('title', 'file')
+        fields = ('title', 'file', 'has_rights')
+
+    has_rights = forms.BooleanField(label=_("I have all the rights to upload this track"),
+                                    help_text=_("By checking this box, you certify that you have the rights to upload this track and that you are not belonging to a company that denies this."),
+                                    required=True)
 
 class PresskitViewRequestForm(forms.ModelForm):
     class Meta:
