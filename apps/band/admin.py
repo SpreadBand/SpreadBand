1c79181f2af51447e42284f6d94ec7eb49dfb9ad
Guillaume "dukez" Libersat <guillaume@spreadband.com>
2011-04-05 12:39:44 +0200

    register Band for reversion using admin

--- apps/band/admin.py
+++ apps/band/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
+from reversion.admin import VersionAdmin
+
 from .models import Band, BandRole, BandMember, BandPicture
 
-admin.site.register((Band, BandRole, BandMember, BandPicture))
+class BandAdmin(VersionAdmin):
+    pass
+
+admin.site.register(Band, BandAdmin)
+
+admin.site.register((BandRole, BandMember, BandPicture))
 
 
 
